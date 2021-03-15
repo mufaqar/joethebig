dd3d9f780b33128a33595a41a01d7dffd40b4e82
Mufaqar Islam <mufaqar@gmail.com>
2021-03-15 20:45:56 +0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,7 +38,8 @@ const Contact = () => {
                 </div>
                 <h2 className="contact-info-one__title">Contact Info</h2>
                 <p className="contact-info-one__text">
-                  needhelp@kipso.com <br />
+                  info@joethebig
+.com <br />
                   444 888 0000 <br /> &nbsp;
                 </p>
               </div>
