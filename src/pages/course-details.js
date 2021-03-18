15e20e79235def5917fe85b23f5face015a3a674
Mufaqar Islam <mufaqar@gmail.com>
2021-03-18 19:43:24 +0500

    top bar

--- src/pages/course-details.js
+++ src/pages/course-details.js
@@ -4,10 +4,12 @@ import NavOne from "../components/NavOne";
 import Footer from "../components/Footer";
 import CourseDetails from "../components/CourseDetails";
 import RelatedProducts from "../components/RelatedProducts";
+import Topbar from "../components/Topbar";
 
 const CoursesPage = () => {
     return (
         <Layout pageTitle="Joe The Big | Course Details">
+                <Topbar/>
             <NavOne />
            <CourseDetails />             
             <RelatedProducts />            
--- src/pages/courses.js
+++ src/pages/courses.js
@@ -4,10 +4,11 @@ import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
 import Courses from "../components/Courses";
-
+import Topbar from "../components/Topbar";
 const CoursesPage = () => {
     return (
         <Layout pageTitle="Joe The Big | Courses">
+               <Topbar /> 
             <NavOne />
             <PageHeader title="Products" />
             <Courses />
