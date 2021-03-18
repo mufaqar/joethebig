fa84874b08e99c2bf8eebe3ad51ff40b97dc388e
Mufaqar Islam <mufaqar@gmail.com>
2021-03-18 15:18:12 +0500

    Updated product page

--- src/pages/course-details.js
+++ src/pages/course-details.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
-import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
 import CourseDetails from "../components/CourseDetails";
+
 
 const CoursesPage = () => {
     return (
         <Layout pageTitle="Joe The Big | Course Details">
             <NavOne />
-            <PageHeader title="Product Details" />
-         
+           <CourseDetails />              
             <Footer />
         </Layout>
     );
